4a14929337dac6762ed320b1248d80d1945112fc
unknown <maria.bratan@student.upt.ro>
2021-03-13 11:04:04 +0200

    Start

---
+++ VirtualMarket/src/test/java/com/example/VirtualMarket/VirtualMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VirtualMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
